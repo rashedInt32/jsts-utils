aa95acc7c70788b828a1edebe89b7dd9720e58ac
rashed parvez <parvez08eee@gmail.com>
2025-02-27 16:33:42 +0600

    turbo custom generator and added try-catch package

---
+++ turbo/generators/config.ts
@@ -0,0 +1,41 @@
+import type { PlopTypes } from "@turbo/gen";
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setGenerator("generate-package", {
+    description: "Generate package",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "what is the name of the pakcage?",
+      },
+      {
+        type: "input",
+        name: "deps",
+        message:
+          "Enter a space separated list of dependencies yhou would like to install",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "packages/{{name}}/eslint.config.js",
+        templateFile: "templates/eslint.config.js.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/{{name}}/package.json",
+        templateFile: "templates/package.json.hbs",
+      },
+      {
+        type: "add",
+        path: "packages/{{name}}/index.ts",
+        template: "export * from './src';",
+      },
+      {
+        type: "add",
+        path: "packages/{{name}}/src/index.ts",
+        template: "export const name = '{{name}}';",
+      },
+    ],
+  });
+}
