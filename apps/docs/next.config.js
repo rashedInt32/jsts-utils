9bdfa18ee903617d10bb570a3ff3f3341d0b78ec
rashed parvez <parvez08eee@gmail.com>
2025-03-03 00:21:54 +0600

    Added nextra config and layout

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextra from 'nextra'
 
-export default nextConfig;
+const withNextra = nextra({
+
+})
+
+
+export default withNextra({
+
+});
