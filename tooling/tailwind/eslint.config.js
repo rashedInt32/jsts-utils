85f88280006c95f65d4fecccf404e2aec26df341
rashed parvez <parvez08eee@gmail.com>
2025-03-07 23:34:39 +0600

    removed ts prettier packages from package folder

--- tooling/tailwind/eslint.config.js
+++ tooling/tailwind/eslint.config.js
@@ -1,6 +1,6 @@
 // FIXME: This kinda stinks...
 /// <reference types="../../tooling/eslint/types.d.ts" />
 
-import baseConfig from "@repo/eslint-config/base";
+import baseConfig from "@jsts-utils/eslint-config/base";
 
 export default [...baseConfig];
