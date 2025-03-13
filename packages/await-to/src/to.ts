25f4f154d6b6ac7fafdff21f452f03eea25dc50e
rashed parvez <parvez08eee@gmail.com>
2025-03-13 12:05:15 +0600

    Fixed prettier issue

--- packages/await-to/src/to.ts
+++ packages/await-to/src/to.ts
@@ -1,12 +1,10 @@
-export async function to<T, U = Error>
-  (promise: Promise<T>): Promise<[U, undefined] | [null, T]> {
-  return promise.then<[null, T]>((data: T) => [null, data])
-    .catch<[U, undefined]>((err: U) => [err, undefined])
+export async function to<T, U = Error>(
+  promise: Promise<T>
+): Promise<[U, undefined] | [null, T]> {
+  return promise
+    .then<[null, T]>((data: T) => [null, data])
+    .catch<[U, undefined]>((err: U) => [err, undefined]);
 }
 export function someFunc() {
   const left = "right";
-
-  }
-
-
-
+}
