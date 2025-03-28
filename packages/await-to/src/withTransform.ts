2bdf01885734465f818303ca9912e17f44ef25ff
rashed parvez <parvez08eee@gmail.com>
2025-03-28 23:57:19 +0600

    Added withTransform function

---
+++ packages/await-to/src/withTransform.ts
@@ -0,0 +1,8 @@
+export function withTransform<T, R, U = Error>(
+  promise: Promise<T>,
+  transform: (data: T) => R,
+): Promise<[U, undefined] | [null, R]> {
+  return promise
+    .then<[null, R]>((data) => [null, transform(data)])
+    .catch<[U, undefined]>((err: U) => [err, undefined]);
+}
