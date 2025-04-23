0237641adf61030c0fc82e13505fdb089650ba7b
rashed parvez <parvez08eee@gmail.com>
2025-04-24 00:01:42 +0600

    fixed test

--- packages/await-to/test/withTransform.test.ts
+++ packages/await-to/test/withTransform.test.ts
@@ -1,27 +1,22 @@
 import { describe, expect, it } from "vitest";
-import { withTransform  } from "../src";
+import { withTransform } from "../src";
 
-describe('withTransform', () => {
-  it('should transform the result', async () => {
-    const [err, result] = await withTransform(Promise.resolve(42), data => data * 2);
+describe("withTransform", () => {
+  it("should transform the result", async () => {
+    const [err, result] = await withTransform(
+      Promise.resolve(42),
+      (data) => data * 2
+    );
     expect(err).toBeNull();
     expect(result).toBe(84);
   });
 
-  it('should transform the error', async () => {
-    const [err, result] = await withTransform(Promise.reject(new Error('foo'))).catch(err => err.message);
-    expect(err).toBe('foo');
+  it("should transform the error", async () => {
+    const [err, result] = await withTransform(
+      Promise.reject(new Error("foo")),
+      () => {}
+    );
+    expect(err).toBe("foo");
     expect(result).toBeUndefined();
   });
-
-  it('should transform the error and result', async () => {
-    const [err, result] = await withTransform(Promise.resolve(42)).then(x => x * 2).catch(err => err.message);
-    expect(err).toBeNull();
-    expect(result).toBe(84);
-  });
-
-  it('should transform the error and result', async () => {
-    const [err, result] = await withTransform(Promise.resolve(42)).then(x => x * 2).catch(err => err.message);
-    expect(err).toBeNull();
-    expect(result).toBe(84);
-  });
+});
